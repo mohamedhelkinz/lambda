167ed09c556fdfdb6ab5c8e430906a2e244629ca
mohamedhelkinz <mohamed.elhousni@helkinz.com>
2022-08-09 09:38:34 +0200

    add somme changes to test ci

--- hellolambda.js
+++ hellolambda.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify("Hello from Lambda and Github CI from branch lambdanodejs!"),
+      body: JSON.stringify("Hello from Lambda and Github CI from branch lambdanodejs amin!"),
     }
     return response}
